3411d0373ff42ca7f1b940fb0f923f10138d61bf
aliyaseitova <230587@astanait.edu.kz>
2024-05-12 14:31:10 +0500

    declared variables and getters in MyTestingClass

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,3 +1,16 @@
-public class MyTestingClass {
+public class MyTestingClass { //creating new class for testing MyHashTable
+private int id; //new variable id
+private String name;//new variable name
 
+public MyTestingClass(int it, String name) {
+    this.id = id;
+    this.name = name;
+}
+
+public int getId(){
+    return id; //getter retrieves and returns id
+}
+public String getName(){
+    return name; // getter retrieves and returns name
+}
 }

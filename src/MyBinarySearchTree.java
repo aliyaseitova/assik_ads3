f45ba4ac740909a23965a3a8a1baded664a88e1f
aliyaseitova <230587@astanait.edu.kz>
2024-05-15 20:55:22 +0500

    Created MyBinarySearchTree java class, declared some methods

---
+++ src/MyBinarySearchTree.java
@@ -0,0 +1,15 @@
+public class MyBinarySearchTree<K extends Comparable<K>, V>{
+    private class Node root;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+    }
+    public void put(K key, V val){}
+    private Node put(Node x, K key, V value) {}
+    public V get (K key){}
+    public void delete (K key){}
+}
+
+

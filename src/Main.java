9742d80ec0bab303748ea5c13652b6a3ebdbea2b
aliyaseitova <230587@astanait.edu.kz>
2024-05-16 10:37:27 +0500

    Implemented whole Main code

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import com.sun.source.tree.BinaryTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.insert(5);
+        binaryTree.insert(3);
+        binaryTree.insert(4);
+        binaryTree.insert(2);
+        binaryTree.insert(6);
+        binaryTree.insert(7);
+
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data before remove: " +binaryTree.root.data);
+        binaryTree.remove(5);
+        System.out.println();
+        binaryTree.inOrder();
+        System.out.println();
+        System.out.println("Root data after remove: " +binaryTree.root.data);
+    }
+}

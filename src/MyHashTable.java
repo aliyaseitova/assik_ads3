ad654dce59c1001c2fcd6962c3de10a9af11ddbd
aliyaseitova <230587@astanait.edu.kz>
2024-05-16 10:39:44 +0500

    Added more methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -89,8 +89,28 @@ public class MyHashTable<K, V> {
     }
 
     public boolean contains(V value) {
+        for(int i=0; i<M; i++){
+            HashNode<K, V> node = chainArray[i];
+            while (node != null){
+                if (node.value == value){
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
     }
 
     public K getKey(V value) {
+        for(int i=0; i<M; i++){
+            HashNode<K, V> node = chainArray[i];
+            while (node != null){
+                if (node.value == value){
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
     }
 }

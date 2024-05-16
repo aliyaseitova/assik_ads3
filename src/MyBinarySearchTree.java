58e7237eda99d7c1e27bf1a9a1a2722632cdf7a6
aliyaseitova <230587@astanait.edu.kz>
2024-05-16 10:07:11 +0500

    Implemented whole MyBinarySearchTree code

--- src/MyBinarySearchTree.java
+++ src/MyBinarySearchTree.java
@@ -1,15 +1,74 @@
-public class MyBinarySearchTree<K extends Comparable<K>, V>{
-    private class Node root;
+public class MyBinarySearchTree {
+    public Node root;
 
-    private class Node {
-        private K key;
-        private V value;
-        private Node left, right;
+    public class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
     }
-    public void put(K key, V val){}
-    private Node put(Node x, K key, V value) {}
-    public V get (K key){}
-    public void delete (K key){}
+
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    private Node insert(Node current, int data) {
+        if (current == null)
+            return new Node(data);
+        if (data < current.data)
+            current.left = insert(current.left, data);
+        else
+            current.right = insert(current.right, data);
+        return current;
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void remove(int data) {
+        root = remove(root, data);
+    }
+
+    private Node remove(Node current, int data) {
+        if (current == null)
+            return null;
+        if (data < current.data)
+            current.left = remove(current.left, data);
+        else if (data > current.data)
+            current.right = remove(current.right, data);
+        else {
+            //case 1: no child
+            if (current.left == null && current.right == null)
+                return null;
+
+            //case 2: only one child
+
+            if (current.left == null)
+                return current.right;
+
+            if (current.right == null)
+                return current.left;
+
+            int smallestValue = findSmallestValue(current.left);
+            current.data =smallestValue;
+            current.left = remove(current.left, smallestValue);
+        }
+        return current;
+    }
+
+    private int findSmallestValue(Node node) {
+        return node.right == null ? node.data : findSmallestValue(node.right);
+    }
 }
-
-

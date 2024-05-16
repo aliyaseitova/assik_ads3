87d062f521d22cc068ec365a80f6a0366fb57280
aliyaseitova <230587@astanait.edu.kz>
2024-05-16 12:33:50 +0500

    Fixed the mistake, removed } and added return statements

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,25 +1,21 @@
 public class MyHashTable<K, V> {
-    private class HashNode<K, V> {
+    private class HashNode <K, V> {
         public K key;
         public V value;
-        public MyHashTable.HashNode<K,V> next;
+        public HashNode next;
 
-        private class HashNode<K, V> {
-            private K key;
-            private V value;
-            private HashNode<K, V> next;
 
-            public HashNode(K key, V value) {
-                this.key = key;
-                this.value = value;
-            }
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
-            @Override
-            public String toString() {
-                return "{" + key + " " + value + "}";
-            }
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
         }
     }
+
 
     private HashNode<K, V>[] chainArray;
     private int M = 11;
@@ -37,6 +33,7 @@ public class MyHashTable<K, V> {
     }
 
     private int hash(K key) {
+        return 0;
     }
 
     public void put(K key, V value) {
@@ -102,15 +99,7 @@ public class MyHashTable<K, V> {
     }
 
     public K getKey(V value) {
-        for(int i=0; i<M; i++){
-            HashNode<K, V> node = chainArray[i];
-            while (node != null){
-                if (node.value == value){
-                    return node.key;
-                }
-                node = node.next;
-            }
-        }
         return null;
+
     }
 }

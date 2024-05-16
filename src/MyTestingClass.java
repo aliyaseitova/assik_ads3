d1ceac63d168c9caa8f7a7cedd166c274090d24f
aliyaseitova <230587@astanait.edu.kz>
2024-05-16 11:48:36 +0500

    Added more methods

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class MyTestingClass { //creating new class for testing MyHashTable
 private int id; //new variable id
 private String name;//new variable name
@@ -13,4 +15,16 @@ public int getId(){
 public String getName(){
     return name; // getter retrieves and returns name
 }
+
+@Override
+    public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    MyTestingClass that = (MyTestingClass) o;
+    return id == that.id && Objects.equals(name, that.name);
+}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
